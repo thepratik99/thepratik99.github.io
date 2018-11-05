69e306b2b2afb59475c3565241a6eba658b39377
Pratik Patel <p.pratik1099@gmail.com>
2018-11-05 00:47:24 -0600

    Some last edits

--- bike_share.r
+++ bike_share.r
@@ -1,93 +1,118 @@
-setwd("~/Documents/Bike_Share/bikeshare/src")
+"Put the path of your working directory in the quotes"
+setwd("../src")
 
+"Read the csv file and store it in the variable data"
 data = read.csv("metro-bike-share-trip-data.csv")
+"number of rows in data"
+row <- nrow(data)
+
 startingID <- data[ ,5]
+"make a table of the vector it will give the frequency of each Station"
 startingID.t1 <- table(startingID)
-startingID.t1
-write.table(endingID.t1, "~/Documents/Bike_Share/bikeshare/src/table5.txt", sep="\t",  row.names = TRUE, col.names = TRUE)
+"make a csv file of the table so that we can sort it and find most popular stations"
+write.table(startingID.t1, "~/Documents/BikeShare/startingID.csv", sep="\t",  row.names = TRUE, col.names = TRUE)
 
 endingID <- data[ ,8]
+"make a table of the vector it will give the frequency of each Station"
 endingID.t1 <- table(endingID)
-endingID.t1
-write.table(startingID.t1, "~/Documents/Bike_Share/bikeshare/src/table6.txt", sep="\t",  row.names = TRUE, col.names = TRUE)
+"make a csv file of the table so that we can sort it and find most popular stations"
+write.table(endingID.t1, "~/Documents/BikeShare/endingID.csv", sep="\t",  row.names = TRUE, col.names = TRUE)
 
-pass <- data[ ,14]
-pass.t1 <- table(pass)
-pass.t1
-
+"get the duration column in the duration vector"
 duration <- data[ ,2]
-sum = 0;
-for(i in 1:132427)
-{
-  sum = sum + ((duration[i] / 3600 )* 10)
-}
-avgD <- sum/132427
-avgD
-
+"taking a mean of the vector"
 m1 <- mean(duration)
-dur <- (m1 /3600) * 10
-dur
+"Suppose that the average biking speed is 10kmph
+  Since time we have given is in seconds divide the mean by 3600 and multiply 10
+  multiply by 5/8 to get the distance in miles"
+distance <- ((m1 /3600) * 10) * (5/8)
+distance
 
 date <- data[ ,3]
-dats <- vector()
-
-for(i in 1:132427)
-{
-  dats[i] = substring(date[i], 1, 10)
-}
-dats.t1 <- table(dats)
-dats.t1
-hist(dats)
-
-meanDuration <- vector()
-
-count = 1
-
-for(i in 1:268)
-{
-  meanDuration[i] = 0;
-  for(j in 1:dats.t1[[i]])
-  {
-    meanDuration[i] = meanDuration[i] + (duration[count] / 60)
-    count = count +1
-  }
-  meanDuration[i] = meanDuration[i] / dats.t1[[i]]
-}
-
+"make a substring of each string in the date vector to only get the date"
 dates <- vector()
-counter = 1
-
-for(i in 1:268)
+for(i in 1:row)
 {
-  for(j in 1:dats.t1[[i]])
-  {
-    dates[i] = dats[counter]
-    counter = counter +1
-  }
+  dates[i] = substring(date[i], 1, 10)
+}
+"making a table will help us figure out the frequency of each date"
+dates.t1 <- table(dates)
+numberOfDays <- nrow(dates.t1)
+
+"make a vector to store the number of trips on each day"
+numberOfTrips <- vector()
+for(i in 1:numberOfDays)
+{
+  numberOfTrips[i] = dates.t1[[i]]
 }
 
-write.table(dats.t1, "~/Documents/Bike_Share/bikeshare/src/table1.txt", sep="\t",  row.names = TRUE, col.names = TRUE)
-
-write.table(dates, "~/Documents/Bike_Share/bikeshare/src/table2.txt", sep="\t",  row.names = FALSE, col.names = FALSE)
-write.table(meanDuration, "~/Documents/Bike_Share/bikeshare/src/table3.txt", sep="\t",  row.names = FALSE, col.names = FALSE)
+"make a vector for the dates that will be same size of numberOfTrips vector"
+dats <- vector()
+counter = 1
+for(i in 1:numberOfDays)
+{
+  dats[i] = dates[counter]
+  counter = counter + dates.t1[[i]]
+}
 
 trip <- data[ ,13]
 pass <- data[ ,14]
 
-trip.flex <- vector()
-iCount = 1
-for(i in 1:132427)
+"make vectors to store the number of trips done with certain type"
+dayFlexV <- vector()
+dayMonthV <- vector()
+dayWalkV <- vector()
+dayStaffV <- vector()
+
+"Edit the vector according to given data set"
+count = 1
+for(i in 1:numberOfDays)
 {
-  if(pass[i] == "Flex Pass")
+  dayFlexV[i] = 0
+  dayMonthV[i] = 0
+  dayWalkV[i] = 0
+  dayStaffV[i] = 0
+  for(j in 1:dates.t1[[i]])
   {
-    trip.flex[iCount] = trip[i]
-    iCount = iCount + 1
+    if(pass[count] == "Flex Pass")
+    {
+      dayFlexV[i] = dayFlexV[i] + 1
+    }
+    else if(pass[count] == "Monthly Pass")
+    {
+      dayMonthV[i] = dayMonthV[i] + 1
+    }
+    else if(pass[count] == "Walk-up")
+    {
+      dayWalkV[i] = dayWalkV[i] + 1
+    }
+    else
+    {
+      dayStaffV[i] = dayStaffV[i] + 1
+    }
+    count = count + 1
   }
 }
 
-flexType <- vector()
+"make a data frame and export it as a csv file to analysize the data and graph it"
+passType.table = data.frame(dats,numberOfTrips, dayFlexV, dayMonthV, dayWalkV, dayStaffV)
+write.table(passType.table, "~/Documents/BikeShare/typesOfPasses.csv", sep="\t",  row.names = TRUE, col.names = TRUE)
 
-for(i in 1:9517)
+"make a vector to store the type of the trip only with flex pass"
+trip.flex <- vector()
+flexCount = 1
+for(i in 1:row)
+{
+  if(pass[i] == "Flex Pass")
+  {
+    trip.flex[flexCount] = trip[i]
+    flexCount = flexCount + 1
+  }
+}
+
+"convert 1 and 2's to One Way or Round Trip and make a table to get frequency"
+flexType <- vector()
+for(i in 1:(flexCount-1))
 {
   if(trip.flex[i] == 1)
   {
@@ -98,24 +123,23 @@ for(i in 1:9517)
     flexType[i] = "Round Trip"
   }
 }
-
 flexType.t1 <- table(flexType)
-flexType.t1
 
+"make a vector to store the type of the trip only with monthly pass"
 trip.monthly <- vector()
-iCounter = 1
-for(i in 1:132427)
+monthCount = 1
+for(i in 1:row)
 {
   if(pass[i] == "Monthly Pass")
   {
-    trip.monthly[iCounter] = trip[i]
-    iCounter = iCounter + 1
+    trip.monthly[monthCount] = trip[i]
+    monthCount = monthCount + 1
   }
 }
 
+"convert 1 and 2's to One Way or Round Trip and make a table to get frequency"
 monthlyType <- vector()
-
-for(i in 1:81304)
+for(i in 1:(monthCount-1))
 {
   if(trip.monthly[i] == 1)
   {
@@ -126,24 +150,23 @@ for(i in 1:81304)
     monthlyType[i] = "Round Trip"
   }
 }
-
 monthlyType.t1 <- table(monthlyType)
-monthlyType.t1
 
+"make a vector to store the type of the trip only with walk-ups"
 trip.daily <- vector()
-jCounter = 1
-for(i in 1:132427)
+dailyCount = 1
+for(i in 1:row)
 {
   if(pass[i] == "Walk-up")
   {
-    trip.daily[jCounter] = trip[i]
-    jCounter = jCounter + 1
+    trip.daily[dailyCount] = trip[i]
+    dailyCount = dailyCount + 1
   }
 }
 
+"convert 1 and 2's to One Way or Round Trip and make a table to get frequency"
 dailyType <- vector()
-
-for(i in 1:41224)
+for(i in 1:(dailyCount-1))
 {
   if(trip.daily[i] == 1)
   {
@@ -154,80 +177,8 @@ for(i in 1:41224)
     dailyType[i] = "Round Trip"
   }
 }
-
 dailyType.t1 <- table(dailyType)
-dailyType.t1
 
-dayFlexV <- vector()
-dayMonthV <- vector()
-dayWalkV <- vector()
-dayStaffV <- vector()
-
-kCounter = 1
-for(i in 1:268)
-{
-  dayFlexV[i] = 0
-  dayMonthV[i] = 0
-  dayWalkV[i] = 0
-  dayStaffV[i] = 0
-  for(j in 1:dats.t1[[i]])
-  {
-    if(pass[kCounter] == "Flex Pass")
-    {
-      dayFlexV[i] = dayFlexV[i] + 1
-    }
-    else if(pass[kCounter] == "Monthly Pass")
-    {
-      dayMonthV[i] = dayMonthV[i] + 1
-    }
-    else if(pass[kCounter] == "Walk-up")
-    {
-      dayWalkV[i] = dayWalkV[i] + 1
-    }
-    else
-    {
-      dayStaffV[i] = dayStaffV[i] + 1
-    }
-    kCounter = kCounter + 1
-  }
-}
-
-passType.table = data.frame(dates, dayFlexV, dayMonthV, dayWalkV, dayStaffV)
-write.table(passType.table, "~/Documents/Bike_Share/bikeshare/src/table3.txt", sep="\t",  row.names = FALSE, col.names = FALSE)
-
-bikeID <- data[ ,11]
-bikeID.t1 <- table(bikeID)
-bikeID.t1
-
-startDayOne <- vector()
-
-for(i in 1:190)
-{
-  startDayOne[i] = startingID[i]
-}
-
-startDayOne.t1 <- table(startDayOne)
-startDayOne.t1
-
-mat<-matrix(NA, ncol = 2, nrow = )
-count = 0
-
-for(j in 1:268)
-{
-  endDayOne <- vector()
-  for(i in 1:dates[i])
-  {
-    endDayOne[i] = endingID[count]
-  }
-  endDayOne.t1 <- table(endDayOne)
-  endDayOne.t1
-  for(k in 1:nrow(endDayOne.t1))
-  {
-    
-  }
-}
-nrow(endDayOne.t1)
-
-endDayOne.t1[[1]]
-
-
+"make a data frame of the frequency table and export it as a csv file so that we can make a graph"
+type <- data.frame(flexType.t1, monthlyType.t1, dailyType.t1)
+write.table(type, "~/Documents/BikeShare/TripRoute-Pass.csv", sep="\t",  row.names = FALSE, col.names = TRUE)
